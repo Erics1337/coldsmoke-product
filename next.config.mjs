c4b0747f7431c6f0ddd83bd2c64a722eee242887
Eric <erics1337@gmail.com>
2025-06-06 20:33:10 -0600

    fix(next.config): adjust basePath and assetPrefix for GitHub Pages
    Ensure static assets and routing work correctly in production by setting empty string values for basePath and assetPrefix in development

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,9 @@ const isProd = process.env.NODE_ENV === 'production';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  // basePath: isProd ? '/coldsmoke-product' : '', // Still needed for routing
-  assetPrefix: isProd ? '/coldsmoke-product' : undefined,
-  basePath: isProd ? '/coldsmoke-product' : undefined,
-    images: {
+  basePath: isProd ? '/coldsmoke-product' : '', // Still needed for routing
+  assetPrefix: isProd ? '/coldsmoke-product' : '', // Crucial for static assets on GitHub Pages
+  images: {
     unoptimized: true,
   },
   // Optional: Add a trailing slash to all URLs
